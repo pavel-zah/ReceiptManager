ea0c6a0cd43bd1b398c2ee118802dd49a4454adb
paupaupau <94736048+peredz@users.noreply.github.com>
2026-03-13 00:05:24 +1000

    refactor: migrate project to monorepo structure

--- apps/ocr-service/infrastructure/ai/ocr_service_impl.py
+++ apps/ocr-service/infrastructure/ai/ocr_service_impl.py
@@ -9,6 +9,14 @@ SYSTEM_PROMPT = """
                   записывай ошибку ее в поле error, а позицию пропускай, если ошибок не было то оставляешь поле пустым.
                  """
 
+
+SYSTEM_PROMPT_FOR_TEXT = """
+                 Ты - полезный агент, специалист по распознаванию чеков.
+                 Твоя основная задача заключается в извлечении из текста чека всех позиций в заказе с указанием их цены.
+                 -Если при распознавании чека произошла ошибка,то ты ничего не придумываешь,
+                  записывай ошибку ее в поле error, а позицию пропускай, если ошибок не было то оставляешь поле пустым.
+                 """
+
 class LMStudioModel(OCRService):
     """Адаптер для взаимодействия с локальной VL-моделью через LM Studio API
 
@@ -38,3 +46,25 @@ class LMStudioModel(OCRService):
         except Exception as e:
             error_msg = str(e)
             return ParsedReceiptDTO(error=error_msg)
+
+    def process_receipt_text(self, receipt_text: str) -> ParsedReceiptDTO:
+        """Обрабатывает текст чека, извлекая структурированные данные
+
+        Использует LM Studio для отправки изображения и системного промпта
+        модели, парсит ответ в Pydantic DTO."""
+        chat = lms.Chat(initial_prompt=SYSTEM_PROMPT_FOR_TEXT)
+        prompt = f"""
+        Распознай все позиции из этого чека с их ценами,
+        определи дату заказа, сумму чаевых,
+        сервисный сбор (он так же может называться платой за стол или за обслуживание)
+        Текст чека:
+        {receipt_text}"""
+        chat.add_user_message(prompt)
+        try:
+            prediction = self.model.respond(chat, response_format=ParsedReceiptDTO)
+            return prediction.parsed
+        except Exception as e:
+            error_msg = str(e)
+            return ParsedReceiptDTO(error=error_msg)
+
+

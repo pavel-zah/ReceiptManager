ecfe358ad586c3a87e831848341549b0b0ed2ea7
paupaupau <94736048+peredz@users.noreply.github.com>
2026-03-13 12:27:18 +1000

    fix(db-api): edit dependencies feat(db): add init.sql for postgres docker

--- apps/db-service/database.py
+++ apps/db-service/database.py
@@ -3,10 +3,26 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/receiptmanager",
+DATABASE_HOST = os.getenv(
+    "DB_HOST",
+    "localhost",
 )
+
+DATABASE_PORT = os.getenv(
+    "DB_PORT",
+    "5432",
+)
+DATABASE_USER = os.getenv(
+    "POSTGRES_USER",
+    "postgres",
+)
+
+DATABASE_PASSWORD = os.getenv(
+    "POSTGRES_PASSWORD",
+    "postgres",
+)
+
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

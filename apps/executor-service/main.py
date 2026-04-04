77029a64271d537fd2c0e8ac6de2ca745c774085
paupaupau <94736048+peredz@users.noreply.github.com>
2026-04-04 10:31:07 +1000

    feat: add dependencies and router

--- apps/executor-service/main.py
+++ apps/executor-service/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.api.routers import api_router
+from app.api import router
 from app.clients.db_client import DBClient
+from app.agent.agent import get_agent
+import uvicorn
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.db_client = DBClient()
+    app.state.agent = get_agent()
     yield
     await app.state.db_client.close()
 
-def create_app() -> FastAPI:
-    app = FastAPI(title="Executor service", version="1.0.0")
 
-    app.include_router(api_router.router)
+def create_app() -> FastAPI:
+    app = FastAPI(title="Executor service", version="1.0.0", lifespan=lifespan)
+
+    #TODO change name, not router.router
+    app.include_router(router.router)
 
     @app.get("/health")
     def health():
@@ -21,3 +27,6 @@ def create_app() -> FastAPI:
     return app
 
 app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8002)

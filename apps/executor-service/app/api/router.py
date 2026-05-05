d222d9af78be44130fb31fe9c6ab6f982d170707
paupaupau <94736048+peredz@users.noreply.github.com>
2026-05-06 00:18:13 +1000

    feat: add logger

--- apps/executor-service/app/api/router.py
+++ apps/executor-service/app/api/router.py
@@ -3,6 +3,11 @@ from app.api.schemas import ChatRequest
 from app.api.dependencies import DB, LLM, Agent, ReceiptAgent, Graph
 from langchain_core.runnables import RunnableConfig
 from app.agent.schemas import AgentRoomState
+from app.core.logger import get_logger
+
+
+logger = get_logger(__name__)
+
 
 
 router = APIRouter(prefix="/receipt", tags=["receipt agent commands"])
@@ -15,7 +20,6 @@ async def chat_with_agent(
         # llm: LLM,
         agent: ReceiptAgent,
         ) -> dict:
-    # Создаем объект состояния, который будем отслеживать
     # TODO: Добавить обработку состояний - отправка на фронт сообщений
     config = RunnableConfig(
         configurable = {
@@ -28,54 +32,15 @@ async def chat_with_agent(
         }
     )
 
-    # Запускаем агента
     response = await agent.ainvoke(
         request.user_message,
         config=config
     )
-    print(response)
+    logger.info(response)
     answer = response.get("answer", {})
 
 
-    # Формируем ответ для фронтенда
     return {
         "answer": answer,
-        "action_required": response.get("receipt_updated") if response.get("receipt_updated") else "None, ничо не передало"
+        "action_required": response.get("receipt_updated") if response.get("receipt_updated") else "None"
     }
-
-
-#
-# @router.post("/chat")
-# async def chat_with_agent(
-#         request: ChatRequest,
-#         db: DB,
-#         llm: LLM,
-#         agent: Agent,
-#         graph: Graph
-#         ):
-#     # Создаем объект состояния, который будем отслеживать
-#     # TODO: Добавить обработку состояний - отправка на фронт сообщений
-#     config = RunnableConfig(
-#         configurable = {
-#             "db_client": db,
-#             "llm": llm,
-#             "agent": agent,
-#             "thread_id": f"{request.user_id}:{request.receipt_id}"},
-#         metadata = {
-#             "user_id": request.user_id,
-#             "receipt_id": request.receipt_id
-#         }
-#     )
-#
-#     # Запускаем агента
-#     response = await graph.ainvoke(
-#         { "messages": [request.user_message] },
-#         config=config
-#     )
-#     print(response)
-#
-#     # Формируем ответ для фронтенда
-#     return {
-#         "answer": response.get("answer", "None"),
-#         "action_required": "update_receipt" if response.get("receipt_updated") else "None, ничо не передало"
-#     }
